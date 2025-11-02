43453f63f528f7e403446d90bdd9d749179da7f7
Shubham Mohapatra <mshubhamm2005@gmail.com>
2025-11-02 20:04:07 +0530

    chore: Finalize seeder configuration and provider registration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            User::class,
+            UserSeeder::class,
             OrganizationSeeder::class,
             ContactSeeder::class,
         ]);
