c5872f84ccaca9eb76107df54c6e667c48749b16
Shubham Mohapatra <mshubhamm2005@gmail.com>
2025-11-04 15:11:13 +0530

    chore: Add development tools and finalize configuration
    - Installs and configures the Laravel Debugbar for enhanced local debugging.
    - Updates composer and npm dependencies.
    - Integrates toast notifications into the main admin layout for flash messages.

--- resources/js/Layouts/AdminLayout.tsx
+++ resources/js/Layouts/AdminLayout.tsx
@@ -1,41 +1,13 @@
-import { usePage } from '@inertiajs/react';
 import { motion } from 'framer-motion';
-import { ReactNode, useEffect } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
+import { ReactNode } from 'react';
 import Footer from '../Components/Admin/Footer.js';
 import Navbar from '../Components/Admin/Navbar.js';
-import { PageProps } from '../types.js';
 
 const AdminLayout = ({ children }: { children: ReactNode }) => {
-    const { auth } = usePage<PageProps>().props;
-    const user = auth.user;
-
-    // useEffect(() => {
-    //     if (user) {
-    //         window.Echo.private(`user.${user.id}`).listen('UserProfileUpdated', (event: any) => {
-    //             console.log('Real-time event received: ', event);
-    //             toast.success(event.message);
-    //         });
-    //     }
-
-    //     return () => {
-    //         window.Echo.leave(`user.${user?.id}`);
-    //     };
-    // });
+   
 
     return (
         <>
-            <Toaster
-                position="top-right"
-                toastOptions={{
-                    className: '',
-                    style: {
-                        background: '#064663', // Your dark color
-                        color: '#FFFFFF', // White text
-                        border: '1px solid #ECB365',
-                    },
-                }}
-            />
             <Navbar />
             <div className="min-h-screen bg-[#041C32] px-8 py-12 font-sans text-[#ECB365]">
                 <motion.div
